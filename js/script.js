cf8e62c33710d5e1b4c1e34b2cb6998366808e18
ulisse-fontanot <ulissef99@gmail.com>
2021-02-10 17:54:47 +0100

    manca immagine

--- js/script.js
+++ js/script.js
@@ -2,20 +2,23 @@ var app = new Vue({
   el: "#app",
   data:{
     films: [],
-    query: []
+    query: ""
   },
   methods:{
     searchFilm(){
-      axios.get("https://api.themoviedb.org/3/search/tv"; {
+      axios
+      .get("https://api.themoviedb.org/3/search/movie", {
         params: {
           api_key: '533355437691153b9c338305b62a7ad3',
           query: this.query,
           language: 'it-IT'
         }
-      }).then(result => {
-        this.films = result.data.response;
       })
-      .catch(error => console.log('errore'));
+      .then((result) => {
+        this.films = result.data.results;
+        console.log(result.data);
+      })
+      .catch((error) => console.log('errore'));
     }
   }
 });
