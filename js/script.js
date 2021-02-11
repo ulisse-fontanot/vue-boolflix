1e4a13544bf66f4e8e665c92b748857dbf9eb003
ulisse-fontanot <ulissef99@gmail.com>
2021-02-11 13:03:19 +0100

    milestone 1 2 3 fatte

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 var app = new Vue({
   el: "#app",
   data:{
+    media: [],
     films: [],
+    serie: [],
     query: ""
   },
   methods:{
     searchFilm(){
+      //-----------FILM---------------
       axios
       .get("https://api.themoviedb.org/3/search/movie", {
         params: {
@@ -15,18 +18,40 @@ var app = new Vue({
         }
       })
       .then((result) => {
-        // ----------------FUNZIONI----------------------------
         this.films = result.data.results;
-
+        // ----------------FUNZIONI----------------------------
         result.data.results.forEach(item => {
           item.vote_average = Math.round(item.vote_average / 2);
         });
-
-        console.log(result.data);
-        console.log(this.voto);
         // -----------------FUNZIONI---------------------------
       })
       .catch((error) => console.log('errore'));
+      //-----------FILM---------------
+
+      //-----------SERIE TV---------------
+      axios
+      .get("https://api.themoviedb.org/3/search/tv", {
+        params: {
+          api_key: '533355437691153b9c338305b62a7ad3',
+          query: this.query,
+          language: 'it-IT',
+        }
+      })
+      .then((result) => {
+      this.serie = result.data.results;
+      // ----------------FUNZIONI----------------------------
+      this.media = this.films.concat(this.serie);
+
+      result.data.results.forEach(item => {
+        item.vote_average = Math.round(item.vote_average / 2);
+      });
+
+      console.log(result.data);
+      // -----------------FUNZIONI---------------------------
+      })
+      .catch((error) => console.log('errore'));
+      //-----------SERIE TV---------------
+
     }
   }
 });
