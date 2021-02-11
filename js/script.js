d6792a9f4c78d5d5bf66a44a94f1244a229ebe5a
ulisse-fontanot <ulissef99@gmail.com>
2021-02-11 11:18:04 +0100

    stelle fatte

--- js/script.js
+++ js/script.js
@@ -15,8 +15,16 @@ var app = new Vue({
         }
       })
       .then((result) => {
+        // ----------------FUNZIONI----------------------------
         this.films = result.data.results;
+
+        result.data.results.forEach(item => {
+          item.vote_average = Math.round(item.vote_average / 2);
+        });
+
         console.log(result.data);
+        console.log(this.voto);
+        // -----------------FUNZIONI---------------------------
       })
       .catch((error) => console.log('errore'));
     }
